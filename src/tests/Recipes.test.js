dbed735cfba237e4baec7c8b8ff3af0d6ede84cf
Murilo-MRS <murilosantana1993@gmail.com>
2022-10-31 17:26:44 -0300

    [dev]- Testes Recipes 60%

--- src/tests/Recipes.test.js
+++ src/tests/Recipes.test.js
@@ -1,19 +1,40 @@
-import { screen } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 // import userEvent from '@testing-library/user-event';
 import React from 'react';
 import App from '../App';
+import Provider from '../context/Provider';
 import renderWithRouter from './utils/renderWith';
 
-describe('Teste de botões categorias', () => {
-  test('Bõtoes rederizam em meals', () => {
-    renderWithRouter(<App />, { initialEntries: ['/meals'] });
-    const categoryBtn = screen.getAllByTestId(/-category-filter/i);
-    expect(categoryBtn).toBe(5);
+const mealCategories = ['Beef', 'Breakfast', 'Chicken', 'Dessert', 'Goat', 'All'];
+const drinkCategories = ['Ordinary Drink', 'Cocktail', 'Shake', 'Other/Unknown', 'Cocoa', 'All'];
+
+describe.only('Teste de botões categorias', () => {
+  test('Bõtoes rederizam em meals', async () => {
+    renderWithRouter(<Provider><App /></Provider>, { initialEntries: ['/meals'] });
+
+    await waitFor(() => {
+      mealCategories.forEach((e) => (
+        // screen.getByTestId(`${e}-category-filter`);
+        expect(screen.getByTestId(`${e}-category-filter`)).toBeInTheDocument()
+      ));
+    });
   });
 
-  test('Bõtoes rederizam em drinks', () => {
-    renderWithRouter(<App />, { initialEntries: ['/drinks'] });
-    const categoryBtn = screen.getAllByTestId(/-category-filter/i);
-    expect(categoryBtn).toBe(5);
+  test('Bõtoes rederizam em drinks', async () => {
+    renderWithRouter(<Provider><App /></Provider>, { initialEntries: ['/drinks'] });
+
+    await waitFor(() => {
+      drinkCategories.forEach((e) => (
+        // screen.getByTestId(`${e}-category-filter`);
+        expect(screen.getByTestId(`${e}-category-filter`)).toBeInTheDocument()
+      ));
+    });
+
+    // const categoryBtn = screen.findAllByTestId(/-category-filter/i);
+    // await waitFor(() => {
+    //   expect(categoryBtn).toHaveLength(6);
+    // });
+    // const removeAllBtn = screen.getByTestId('All-category-filter');
+    // expect(removeAllBtn).toBeInTheDocument();
   });
 });
