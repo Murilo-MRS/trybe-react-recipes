8f684d77075fdf531d30a0bad03c22f3c7c7bd8a
Murilo-MRS <murilosantana1993@gmail.com>
2022-10-31 18:51:33 -0300

    [dev]-req-23 -  Testes Recipes 100%

--- src/tests/Recipes.test.js
+++ src/tests/Recipes.test.js
@@ -1,40 +1,41 @@
 import { screen, waitFor } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import App from '../App';
-import Provider from '../context/Provider';
+// import Provider from '../context/Provider';
 import renderWithRouter from './utils/renderWith';
 
 const mealCategories = ['Beef', 'Breakfast', 'Chicken', 'Dessert', 'Goat', 'All'];
 const drinkCategories = ['Ordinary Drink', 'Cocktail', 'Shake', 'Other/Unknown', 'Cocoa', 'All'];
 
-describe.only('Teste de botões categorias', () => {
+describe('Teste de botões categorias', () => {
   test('Bõtoes rederizam em meals', async () => {
-    renderWithRouter(<Provider><App /></Provider>, { initialEntries: ['/meals'] });
+    renderWithRouter(<App />, { initialEntries: ['/meals'] });
+
+    mealCategories.forEach(async (e) => (
+      expect(await screen.findByTestId(`${e}-category-filter`)).toBeInTheDocument()
+    ));
+  });
+
+  test('Bõtoes rederizam em drinks', async () => {
+    renderWithRouter(<App />, { initialEntries: ['/drinks'] });
 
     await waitFor(() => {
-      mealCategories.forEach((e) => (
-        // screen.getByTestId(`${e}-category-filter`);
+      drinkCategories.forEach((e) => (
         expect(screen.getByTestId(`${e}-category-filter`)).toBeInTheDocument()
       ));
     });
   });
 
-  test('Bõtoes rederizam em drinks', async () => {
-    renderWithRouter(<Provider><App /></Provider>, { initialEntries: ['/drinks'] });
+  test('Testando Botão remove All', () => {
+    renderWithRouter(<App />, { initialEntries: ['/meals'] });
+    const removeAll = screen.getByTestId(/All-category-filter/i);
+    userEvent.click(removeAll);
+  });
 
-    await waitFor(() => {
-      drinkCategories.forEach((e) => (
-        // screen.getByTestId(`${e}-category-filter`);
-        expect(screen.getByTestId(`${e}-category-filter`)).toBeInTheDocument()
-      ));
-    });
-
-    // const categoryBtn = screen.findAllByTestId(/-category-filter/i);
-    // await waitFor(() => {
-    //   expect(categoryBtn).toHaveLength(6);
-    // });
-    // const removeAllBtn = screen.getByTestId('All-category-filter');
-    // expect(removeAllBtn).toBeInTheDocument();
+  test('Testando Botão remove All', () => {
+    renderWithRouter(<App />, { initialEntries: ['/drinks'] });
+    const removeAll = screen.getByTestId(/All-category-filter/i);
+    userEvent.click(removeAll);
   });
 });
