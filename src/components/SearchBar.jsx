2a757de1402cb2b37afaeb46e6c10b5ffb8f1156
Jefferson Felix <jefferson.felix182@gmail.com>
2022-11-02 00:29:04 -0300

    lint e css

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,12 +8,13 @@ import {
   fetchFoodsByFirstLetter,
   fetchFoodsByIngredient,
   // eslint-disable-next-line comma-dangle
-  fetchFoodsByName,
+  fetchFoodsByName
 } from '../services/Api';
+import '../styles/SearchBar.css';
 
 function SearchBar() {
   const { setFoods, setDrinks } = useContext(Context);
-  const [radioValue, setRadioValue] = useState('Ingredient');
+  const [radioValue, setRadioValue] = useState('');
   const [searchInput, setSearchInput] = useState('');
   const history = useHistory();
   const fLetter = 'First letter';
@@ -74,60 +75,68 @@ function SearchBar() {
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        id="search"
-        value={ searchInput }
-        placeholder="Buscar receitas"
-        data-testid="search-input"
-        onChange={ ({ target }) => setSearchInput(target.value) }
-      />
-      <label htmlFor="search-radio">
-        Ingredient
-        <input
-          name="search-radio"
-          value="Ingredient"
-          type="radio"
-          data-testid="ingredient-search-radio"
-          checked={ radioValue === 'Ingredient' }
-          onChange={ ({ target }) => setRadioValue(target.value) }
-        />
-      </label>
-      <label htmlFor="search-radio">
-        Name
-        <input
-          name="search-radio"
-          value="Name"
-          type="radio"
-          data-testid="name-search-radio"
-          checked={ radioValue === 'Name' }
-          onChange={ ({ target }) => setRadioValue(target.value) }
-        />
-      </label>
-      <label htmlFor="search-radio">
-        First letter
-        <input
-          name="search-radio"
-          value="First letter"
-          type="radio"
-          data-testid="first-letter-search-radio"
-          checked={ radioValue === fLetter }
-          onChange={ ({ target }) => setRadioValue(target.value) }
-        />
-      </label>
-      <button
-        type="button"
-        data-testid="exec-search-btn"
-        onClick={
-          history.location.pathname === '/meals'
-            ? handleClickMeals
-            : handleClickDrinks
-        }
-      >
-        Search
-      </button>
-    </div>
+    <section className="search-bar">
+      <form>
+        <label htmlFor="search-input">
+          <input
+            className="search-input"
+            type="text"
+            id="search-input"
+            value={ searchInput }
+            placeholder="Buscar Receita"
+            autoComplete="off"
+            data-testid="search-input"
+            onChange={ ({ target }) => setSearchInput(target.value) }
+          />
+        </label>
+        <div className="radios">
+          <label htmlFor="ingredient-radio">
+            Ingredient
+            <input
+              name="ingredient-radio"
+              value="Ingredient"
+              type="radio"
+              data-testid="ingredient-search-radio"
+              checked={ radioValue === 'Ingredient' }
+              onChange={ ({ target }) => setRadioValue(target.value) }
+            />
+          </label>
+          <label htmlFor="name-radio">
+            Name
+            <input
+              name="name-radio"
+              value="Name"
+              type="radio"
+              data-testid="name-search-radio"
+              checked={ radioValue === 'Name' }
+              onChange={ ({ target }) => setRadioValue(target.value) }
+            />
+          </label>
+          <label htmlFor="first-letter-radio">
+            First letter
+            <input
+              name="first-letter-radio"
+              value="First letter"
+              type="radio"
+              data-testid="first-letter-search-radio"
+              checked={ radioValue === fLetter }
+              onChange={ ({ target }) => setRadioValue(target.value) }
+            />
+          </label>
+        </div>
+        <button
+          type="button"
+          data-testid="exec-search-btn"
+          onClick={
+            history.location.pathname === '/meals'
+              ? handleClickMeals
+              : handleClickDrinks
+          }
+        >
+          Search
+        </button>
+      </form>
+    </section>
   );
 }
 
